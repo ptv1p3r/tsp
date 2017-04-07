454a5e3ac20b340bb00d3f2c3bd75c088c1bfb1c
slyerx <l.moreira20@hotmail.com>
2017-04-07 23:01:33 +0100

    update

--- main.c
+++ main.c
@@ -8,13 +8,13 @@
 // cria ficheiro rota (tour + cost)
 // cria eps com rota grafica, texto tour + text cost + text date/time
 
-typedef struct cidade {
+typedef struct  {
     int cidadeId;
     float x;
     float y;
-};
+} cidade;
 
-float calculaDistancia(struct cidade a, struct cidade b){
+float calculaDistancia(cidade a, cidade b){
     return (float) sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)  );
 }
 
