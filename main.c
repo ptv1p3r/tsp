eff59e5bb7df44999152f6c64e9297c8d2e412cc
v1p3r <pedro.roldan@gmail.com>
2017-04-07 22:49:41 +0100

    fix

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-
+#include <math.h>
 
 // abre ficheiro
 // calcula rota
 // guarda rota
 
+typedef struct cidade {
+    int cidadeId;
+    float x;
+    float y;
+};
 
-
+float calculaDistancia(cidade a, cidade b){
+    return sqrt( (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)  );
+}
 
 int main() {
     printf("Hello, World!\n");
