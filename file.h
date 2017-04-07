8090ec749b1c7e6d14fe3ffe41d67e8c0583c0b6
v1p3r <pedro.roldan@gmail.com>
2017-04-07 22:38:55 +0100

    Initial commit

---
+++ file.h
@@ -0,0 +1,8 @@
+//
+// Created by v1p3r on 07-04-2017.
+//
+
+#ifndef TSP_FILE_H
+#define TSP_FILE_H
+
+#endif //TSP_FILE_H
--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+
+// abre ficheiro
+// calcula rota
+// guarda rota
+
+
+
+
 int main() {
     printf("Hello, World!\n");
     return 0;
