dfbe7b6e44a1afc64c14e5289ddf07af55359a08
Pedro Roldan <pedro.roldan@gmail.com>
2017-04-08 02:10:48 +0100

    eps files fix

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <memory.h>
 #include "eps.h"
 #include "file.h"
 
@@ -23,11 +25,43 @@ float calculaDistancia(cidade a, cidade b){
     return (float) sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)  );
 }
 */
+
+void printHelp() {
+    printf("Uso: tsp -o directory \n\n");
+    printf("Opção           Descrição\n");
+    printf(" -h,--help      Lista completa de comandos\n");
+    printf(" -o directory   Directoria onde se encontra os ficheiros de dados\n");
+}
+
 int main(int argc, char *argv[]) {
+    int count;
+    char *chrDataLocation;
 
-    printf("Hello, World!\n");
+    if (argc > 1) { /* Valida número de argumentos */
 
-    printf("output %s \n", DATAPATH_OUTPUT);
+        for (count=1; count<argc; count++) {
+
+            /* Valida opção --help */
+            if ((strcmp(argv[count], "--help") == 0) || (strcmp(argv[count], "-h") == 0)) {
+                printHelp();
+                exit(EXIT_SUCCESS);
+            }   /* Valida opção -o */
+            else if ((strcmp(argv[count], "-o") == 0)) {
+
+                if (count+1 < argc) {
+                    chrDataLocation = argv[count+1];
+                    printf("input %s \n", chrDataLocation);
+                } else {
+                    printf("Uso: -o [directory]\n");
+                    exit(EXIT_SUCCESS);
+                }
+            }
+        }
+
+    }  else {
+        printf("Uso: --help para uma lista completa de comandos.\n");
+        exit(EXIT_SUCCESS);
+    }
 
     return 0;
 }
